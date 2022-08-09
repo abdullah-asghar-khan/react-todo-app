7eec024f2e3c9f0ccb5e5a42a112b541d6df0870
Abdullah2213565 <dulakhan024@gmail.com>
2022-08-09 13:01:39 +0200

    Fixed linters

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './functionBased/App.css';
-import TodoContainer from './functionBased/components/TodoContainer';
+import TodoContainer from './funtionBased/components/TodoContainer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
